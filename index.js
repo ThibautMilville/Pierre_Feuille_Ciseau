d9d5b98872917e11218f03bae21418d71cf7073a
ThibautMilville <tmilville.pro@gmail.com>
2023-03-01 20:04:50 +0100

    finalisation

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ choixPossibles.forEach(choixPossible => choixPossible.addEventListener('click',(
     choixUtilisateur = e.target.id;
     // On ajoute l'image qui correspond
     contenantChoixUtilisateur.innerHTML = `<img src="${choixUtilisateur}.png">`
+    generer_choix_ordinateur()
 }))
+
+// Fonction pour générer le choix de l'ordinateur
+function generer_choix_ordinateur(){
+    random = Math.floor(Math.random() * 3) +1 // Générer des nombres compris entre 1 et 3
+    if(random === 1){
+        choixOrdinateur = "pierre"
+    }
+    if(random === 2){
+        choixOrdinateur = "feuille"
+    }
+    if(random === 3){
+        choixOrdinateur = "ciseaux"
+    }
+    // On ajoute l'image qui correspond au choix
+    contenantChoixOrdinateur.innerHTML = `<img src="${choixOrdinateur}.png">`
+}
